48f8a79f90c16b3a89d9167c359841e9325990b4
Michael Dawson <email.michaeldawson@gmail.com>
2018-01-27 02:19:29 +1100

    WIP - controller commit

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Controller : MonoBehaviour {
-  public GameObject playerPrefab;
+public GameObject droneSpawnPoints;
+    public GameObject dronePrefab;
+public GameObject playerPrefab;
   public GameObject p1Spawn;
   public GameObject p2Spawn;
   Player player1;
@@ -14,8 +16,14 @@ public class Controller : MonoBehaviour {
     player1.playerNum = 1;
     player2 = GameObject.Instantiate(playerPrefab, p2Spawn.transform.position, p2Spawn.transform.rotation).GetComponent<Player>();
     player2.playerNum = 2;
-	}
-	
+
+        Debug.Log(droneSpawnPoints.transform.GetChild(0));
+        //GameObject[] things = droneSpawnPoints.GetComponents<GameObject>();
+        foreach (GameObject gameObject in droneSpawnPoints.transform) {
+            GameObject.Instantiate(dronePrefab, gameObject.transform.position, gameObject.transform.rotation);
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
