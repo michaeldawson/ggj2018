f56c9aad7e2bb148df8c58f58230d38d3aa3840c
Ed Bassett <ed@devsketchpad.com>
2018-01-27 01:17:13 +1100

    Add game controller and instantiate players from prefab

---
+++ Assets/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+  public GameObject playerPrefab;
+  public GameObject p1Spawn;
+  public GameObject p2Spawn;
+  Player player1;
+  Player player2;
+	// Use this for initialization
+	void Start () {
+    player1 = GameObject.Instantiate(playerPrefab, p1Spawn.transform.position, p1Spawn.transform.rotation).GetComponent<Player>();
+    player1.playerNum = 1;
+    player2 = GameObject.Instantiate(playerPrefab, p2Spawn.transform.position, p2Spawn.transform.rotation).GetComponent<Player>();
+    player2.playerNum = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour {
   public Vector3 direction = Vector3.zero;
   public float turnSpeed = 1f;
   public float acceleration = 10f;
+  public int playerNum;
 	// Use this for initialization
 	void Start () {
 		
@@ -18,14 +19,15 @@ public class Player : MonoBehaviour {
 
   void FixedUpdate() {
     Rigidbody rigidBody = gameObject.GetComponent<Rigidbody>();
-	float deadZone = 0.05f;
-	float x = Input.GetAxis("Horizontal");
-	float z = Input.GetAxis ("Vertical");
-	if (Mathf.Sqrt(Mathf.Pow(x,2f) + Mathf.Pow(z,2f)) > deadZone ) {
-	  direction = (new Vector3 (x, 0f, z)).normalized;
-	  rigidBody.rotation = Quaternion.Slerp(rigidBody.rotation, Quaternion.LookRotation (direction, Vector3.up), 4 * Time.fixedDeltaTime);
-  	rigidBody.AddForce(acceleration * Time.fixedDeltaTime * direction);
-	}
+  	float deadZone = 0.05f;
+    Debug.Log("Horizontal" + playerNum);
+  	float x = Input.GetAxis("Horizontal" + playerNum);
+  	float z = Input.GetAxis ("Vertical" + playerNum);
+  	if (Mathf.Sqrt(Mathf.Pow(x,2f) + Mathf.Pow(z,2f)) > deadZone ) {
+  	  direction = (new Vector3 (x, 0f, z)).normalized;
+  	  rigidBody.rotation = Quaternion.Slerp(rigidBody.rotation, Quaternion.LookRotation (direction, Vector3.up), 4 * Time.fixedDeltaTime);
+    	rigidBody.AddForce(acceleration * Time.fixedDeltaTime * direction);
+  	}
     //rigidBody.rotation = direction = Quaternion.AngleAxis( * Time.fixedDeltaTime * turnSpeed, Vector3.up) * direction;
   }
 }
