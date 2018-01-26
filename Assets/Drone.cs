6832cbff754bf5fcf219cb11fa313c67c9cd48c1
Michael Dawson <email.michaeldawson@gmail.com>
2018-01-27 09:37:47 +1100

    Update drone to reduce the stupid

--- Assets/Drone.cs
+++ Assets/Drone.cs
@@ -5,49 +5,54 @@ using UnityEngine;
 public class Drone : MonoBehaviour {
     public GameObject player;
 
-    public Vector3 direction = Vector3.zero;
     public float acceleration = 10f;
 
+    public float explorationTime = 2.5f;
     public int explorationDistanceUpperBound = 30;
     public int explorationDistanceLowerBound = 5;
 
     public float timeSpentExploring;
 
+    // The target point is the point that the drone is headed towards. Direction represents a vector towards this point.
     public Vector3 targetPoint = Vector3.zero;
+    public Vector3 direction = Vector3.zero;
 
-    void start () {
-        getNewTargetPoint();
+    void Start () {
+        targetPoint = getNewTargetPoint();
     }
 
-    void FixedUpdate() {
+    void Update() {
         Drone drone = GetComponent<Drone>();
-
         Vector3 dronePosition = drone.transform.position;
 
-        Rigidbody rigidBody = drone.GetComponent<Rigidbody>();
-
-        if (timeSpentExploring > 1) {
+        if (timeSpentExploring > explorationTime) {
             timeSpentExploring = 0;
-        } else {
+            targetPoint = getNewTargetPoint();
+        }
+        else {
             timeSpentExploring = timeSpentExploring + Time.fixedDeltaTime;
         }
-
+ 
         float deltaX = targetPoint.x - dronePosition.x;
         float deltaZ = targetPoint.z - dronePosition.z;
 
         direction = (new Vector3(deltaX, 0f, deltaZ)).normalized;
+    }
+
+    void FixedUpdate() {
+        Drone drone = GetComponent<Drone>();
+        Rigidbody rigidBody = drone.GetComponent<Rigidbody>();
         rigidBody.AddForce(acceleration * Time.fixedDeltaTime * direction);
     }
 
+    // Set an 'origin' point to either the drone's position, or the player's position. We then set the 'target' point to somewhere near here. The drone will then head toward this target point.
     Vector3 getNewTargetPoint () {
-        if (player) {
-            Vector3 playerPosition = player.transform.position;
-            // Are we close to our player? If so, set an exploration point some random in some random direction / distance
-            return targetPoint = playerPosition + new Vector3(Random.value * explorationDistanceUpperBound, 0f, Random.value * explorationDistanceUpperBound);
-        } else {
-            Drone drone = GetComponent<Drone>();
-            Vector3 dronePosition = drone.transform.position;
-            return targetPoint = dronePosition + new Vector3(Random.value * explorationDistanceUpperBound, 0f, Random.value * explorationDistanceUpperBound);
-        };
+        Transform newOriginTransform = player ? player.transform : GetComponent<Drone>().transform;
+
+        return newOriginTransform.position + new Vector3(
+            (Random.value - 0.5f) * explorationDistanceUpperBound,
+            0f,
+            (Random.value - 0.5f) * explorationDistanceUpperBound
+        );
     }
 }
