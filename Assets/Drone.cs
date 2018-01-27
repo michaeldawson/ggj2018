be17e847df7c9be2d45c30d648165cbb8d62ac0c
Michael Dawson <email.michaeldawson@gmail.com>
2018-01-27 13:55:43 +1100

    Move drone navigation into ability file

--- Assets/Drone.cs
+++ Assets/Drone.cs
@@ -6,26 +6,13 @@ public class DroneAbilities: System.Collections.Generic.Dictionary<AbilityType, 
 
 public enum AbilityType {
   Combat,
+  DroneNavigation
 };
 
 public class Drone : MonoBehaviour {
   public Player player;
-
-  public float acceleration = 10f;
-
-  public float explorationTime = 2.5f;
-  public int explorationDistanceUpperBound = 30;
-  public int explorationDistanceLowerBound = 5;
-
-  public float timeSpentExploring;
-
-  // The target point is the point that the drone is headed towards. Direction represents a vector towards this point.
-  public Vector3 targetPoint = Vector3.zero;
-  public Vector3 direction = Vector3.zero;
-
   public float initialHealth = 1;
   public float health;
-
   Controller _controller;
   public Controller controller {
     set {
@@ -35,56 +22,27 @@ public class Drone : MonoBehaviour {
       return this._controller;
     }
   }
+
   public DroneAbilities currentAbilities = new DroneAbilities() {
+    { AbilityType.DroneNavigation, 1 }
   };
 
   void Start () {
     this.health = this.initialHealth;
-    this.targetPoint = getNewTargetPoint();
   }
-
-  void Update() {
-    Drone drone = GetComponent<Drone>();
-    Vector3 dronePosition = drone.transform.position;
-
-    if (timeSpentExploring > explorationTime) {
-      timeSpentExploring = 0;
-      targetPoint = getNewTargetPoint();
-    } else {
-      timeSpentExploring = timeSpentExploring + Time.fixedDeltaTime;
-    }
-
-    float deltaX = targetPoint.x - dronePosition.x;
-    float deltaZ = targetPoint.z - dronePosition.z;
-
-    direction = (new Vector3(deltaX, 0f, deltaZ)).normalized;
-
-    if ( currentAbilities.ContainsKey(AbilityType.Combat) ) {
-      gameObject.GetComponent<DroneCombat>().onUpdate(currentAbilities[AbilityType.Combat]);
-    }
-}
-
-  void FixedUpdate() {
-    Drone drone = GetComponent<Drone>();
-    Rigidbody rigidBody = drone.GetComponent<Rigidbody>();
-    rigidBody.AddForce(acceleration * Time.fixedDeltaTime * direction);
-  }
-
-  // Set an 'origin' point to either the drone's position, or the player's position. We then set the 'target' point to somewhere near here. The drone will then head toward this target point.
-  Vector3 getNewTargetPoint () {
-    Transform newOriginTransform = player ? player.transform : GetComponent<Drone>().transform;
-
-    return newOriginTransform.position + new Vector3(
-      (Random.value - 0.5f) * explorationDistanceUpperBound,
-      0f,
-      (Random.value - 0.5f) * explorationDistanceUpperBound
-    );
-  }
-
   public void capturedBy(Player player) {
     this.player = player;
     this.currentAbilities = player.droneAbilities;
     this.GetComponent<Renderer>().material.color = player.colour;
+  }
+
+  void Update() {
+    // TODO: iterate over enum? C# is a bag of dicks
+    if (currentAbilities.ContainsKey(AbilityType.Combat)) {
+      gameObject.GetComponent<DroneCombat>().onUpdate(currentAbilities[AbilityType.Combat]);
+    }
+
+    gameObject.GetComponent<DroneNavigation>().onUpdate(currentAbilities[AbilityType.DroneNavigation]);
   }
 
   public void hitBy(Bullet bullet) {
---
+++ Assets/DroneNavigation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneNavigation : MonoBehaviour {
+  public float acceleration = 150f;
+
+  // The target point is the point that the drone is headed towards
+  public Vector3 targetPoint = Vector3.zero;
+  public int targetPointRandomOffset = 50;
+  float targetPointSetAt;
+
+  // By default, retain a heading for up to 5 seconds, but this should be less when owned by a player for a 'flocking' effect
+  public float targetPointDuration = 2f;
+
+  void Start() {
+    setNewTargetPoint();
+  }
+
+  public void onUpdate(int level) {
+    if (Time.time > targetPointSetAt + targetPointDuration) {
+      targetPointSetAt = Time.time;
+      setNewTargetPoint();
+    }
+
+    Vector3 dronePosition = drone().transform.position;
+    float deltaX = targetPoint.x - dronePosition.x;
+    float deltaZ = targetPoint.z - dronePosition.z;
+
+    Vector3 direction = (new Vector3(deltaX, 0f, deltaZ)).normalized;
+    drone().GetComponent<Rigidbody>().AddForce(acceleration * Time.fixedDeltaTime * direction);
+  }
+
+  void setNewTargetPoint() {
+    Player player = drone().player;
+    Transform newDirectionOrigin = player ? player.transform : drone().transform;
+
+    targetPoint = newDirectionOrigin.position + new Vector3(
+      (Random.value - 0.5f) * targetPointRandomOffset,
+      0f,
+      (Random.value - 0.5f) * targetPointRandomOffset
+    );
+
+    targetPointDuration = Random.value * 4 + 1;
+  }
+  public Drone drone() {
+    return this.gameObject.GetComponent<Drone>();
+  }
+
+}
